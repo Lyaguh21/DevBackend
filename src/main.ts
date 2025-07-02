79411ca37b57dc612b2f9a26273c63603af57919
DESKTOP-822G7LN\vladt <vlad107a@gmail.com>
2025-07-02 15:27:49 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,39 @@ import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Настройка Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // Это имя должно соответствовать тому, что вы используете в @ApiBearerAuth()
+    )
+    .addCookieAuth('refreshToken') // Если используете refresh token в cookies
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
+  // Ваши существующие настройки
   app.enableCors({
     origin: true,
     credentials: true,
@@ -29,5 +58,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Swagger UI is available on: ${await app.getUrl()}/api-docs`);
 }
 bootstrap();
